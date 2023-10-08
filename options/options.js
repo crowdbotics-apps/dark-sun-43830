c3a4045332502d8de7771929744cb557836f9bfb
Crowdbotics <team@crowdbotics.com>
2023-10-08 18:26:56 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dark-sun-43830",
+  url: "https://dark-sun-43830.botics.co",
+  api: "https://dark-sun-43830.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
